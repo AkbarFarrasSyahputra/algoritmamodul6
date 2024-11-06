1c5d03e1f089c0a8c18c24d65c581e1d86a1048e
AkbarFarrasSyahputra <farrassyahputra230426@gmail.com>
2024-11-06 15:13:28 +0700

    Add files via upload

---
+++ PRAK6ALGO.py
@@ -0,0 +1,7 @@
+def hitung_rata_rata(angka1,angka2):
+    rata_rata = (angka1 + angka2) / 2
+    return rata_rata
+angka1 = float(input("angka1 : "))
+angka2 = float(input("angka2 : "))
+jumlah= hitung_rata_rata(angka1,angka2)
+print("Hasil rata_rata:", jumlah)
